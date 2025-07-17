ef5f69e2c56843fae9b3b089c2cc50ee88be8796
ishfak-akbar <ishfakakbar24@gmail.com>
2025-07-18 00:11:20 +0600

    Added CategoryAdapter, PopularAdapter and SliderAdapter class of Adapter package

---
+++ app/src/main/java/com/example/dripwear/Adapter/CategoryAdapter.java
@@ -0,0 +1,67 @@
+package com.example.dripwear.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.dripwear.Domain.CategoryModel;
+import com.example.dripwear.R;
+
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+    private ArrayList<CategoryModel> items;
+    private Context context;
+    private int selectedPosition=-1;
+    private int lastSelectedPosition=-1;
+
+    @NonNull
+    @Override
+    public CategoryAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        ViewholderCategoryBinding binding = ViewholderCategoryBinding.inflate(LayoutInflater.from(context),
+                parent, false);
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryAdapter.ViewHolder holder, int position) {
+        holder.binding.titleTxt.setText(items.get(position).getTitle());
+
+        holder.binding.getRoot().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                lastSelectedPosition=selectedPosition;
+                selectedPosition=position;
+                notifyItemChanged(lastSelectedPosition);
+                notifyItemChanged(selectedPosition);
+            }
+        });
+
+        if(selectedPosition==position){
+            holder.binding.titleTxt.setBackgroundResource(R.drawable.orange_bg);
+            holder.binding.titleTxt.setTextColor(context.getResources().getColor(R.color.white));
+        }else{
+            holder.binding.titleTxt.setBackgroundResource(R.drawable.stroke_bg);
+            holder.binding.titleTxt.setTextColor(context.getResources().getColor(R.color.black));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ViewholderCategoryBinding binding;
+        public ViewHolder(@NonNull View itemView) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
