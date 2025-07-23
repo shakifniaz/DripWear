24ec0b8180c2abdb7b43558eb6bc7892e732bb07
ishrak-saleh <ischy2003@gmail.com>
2025-07-23 16:07:10 +0600

    Clicking on cart icon on the main page crashed the application. It got fixed in CartActivity.java

--- app/src/main/java/com/example/dripwear/Activity/CartActivity.java
+++ app/src/main/java/com/example/dripwear/Activity/CartActivity.java
@@ -25,6 +25,7 @@ public class CartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        binding = ActivityCartBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         managementCart = new ManagmentCart(this);
