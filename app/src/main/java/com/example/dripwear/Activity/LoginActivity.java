2479e3d0f5afe4d751169da850890df1b7052871
shakifniaz <shakifniaz@gmail.com>
2025-08-03 20:08:39 +0600

    added login memory to LoginActivity

--- app/src/main/java/com/example/dripwear/Activity/LoginActivity.java
+++ app/src/main/java/com/example/dripwear/Activity/LoginActivity.java
@@ -43,7 +43,9 @@ public class LoginActivity extends AppCompatActivity {
                 mAuth.signInWithEmailAndPassword(email, password)
                         .addOnCompleteListener(this, task -> {
                             if (task.isSuccessful()) {
-                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
                                 finish();
                             } else {
                                 Exception e = task.getException();
